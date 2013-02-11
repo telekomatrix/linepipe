76c345d98549b755fb547eba99594e7f2527470b
Andreas Wolff <p@andreaswolff.de>
2013-02-11 18:54:29 +0100

    Dryify Linepipe processors

--- lib/linepipe.rb
+++ lib/linepipe.rb
@@ -3,24 +3,26 @@ require_relative "linepipe/process"
 
 module Linepipe
   class << self
+
     def develop(&block)
-      Process.new.tap do |process|
-        process.instance_eval(&block)
-        process.develop
-      end
+      build_process(block) { |process| process.develop }
     end
 
     def run(&block)
-      Process.new.tap do |process|
-        process.instance_eval(&block)
-        process.run
-      end
+      build_process(block) { |process| process.run }
     end
 
     def benchmark(iterations, &block)
+      build_process(block) { |process| process.benchmark(iterations) }
+    end
+
+
+    private
+
+    def build_process(dsl_block, &block)
       Process.new.tap do |process|
-        process.instance_eval(&block)
-        process.benchmark(iterations)
+        process.instance_eval(&dsl_block)
+        yield process
       end
     end
   end
